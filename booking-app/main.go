0365b3466a2d7cea1740fbe27bc93b24629899d4
Pushkar, Deependra <Deependra.Pushkar@dell.com>
2023-04-01 21:16:52 +0530

    loops

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	const conferenceTickets int = 50
@@ -9,28 +12,36 @@ func main() {
 	bookings := []string{}
 	fmt.Printf("Welcome to %v booking application.\nWe have total of %v tickets and %v are still available.\nGet your tickets here to attend\n", conferenceName, conferenceTickets, remainingTickets)
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println("Enter your first name :")
-	fmt.Scanln(&firstName)
+		fmt.Println("Enter your first name :")
+		fmt.Scanln(&firstName)
 
-	fmt.Println("Enter your last name :")
-	fmt.Scanln(&lastName)
+		fmt.Println("Enter your last name :")
+		fmt.Scanln(&lastName)
 
-	fmt.Println("Enter your email :")
-	fmt.Scanln(&email)
+		fmt.Println("Enter your email :")
+		fmt.Scanln(&email)
 
-	fmt.Println("Enter number of tickets :")
-	fmt.Scanln(&userTickets)
+		fmt.Println("Enter number of tickets :")
+		fmt.Scanln(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-	fmt.Printf("These are all our bookings: %v\n", bookings)
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names %v\n", firstNames)
+	}
 
 }
